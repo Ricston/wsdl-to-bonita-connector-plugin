b7e67d67ca78099a4fe29c95d8e9558712644987
Claude Mamo <claude.mamo@ricston.com>
2012-01-09 18:05:30 +0100

    Added wsdl-to-bonita-connector-plugin project

---
+++ src/main/java/com/ricston/bos/wsdltobonitaconnector/cxf/frontend/generator/BonitaConnectorGenerator.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) Ricston Ltd.  All rights reserved.  http://www.ricston.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package com.ricston.bos.wsdltobonitaconnector.cxf.frontend.generator;
+
+import org.apache.cxf.common.i18n.Message;
+import org.apache.cxf.helpers.CastUtils;
+import org.apache.cxf.service.model.ServiceInfo;
+import org.apache.cxf.tools.common.ToolContext;
+import org.apache.cxf.tools.common.ToolException;
+import org.apache.cxf.tools.common.model.*;
+import org.apache.cxf.tools.util.ClassCollector;
+import org.apache.cxf.tools.util.NameUtil;
+import org.apache.cxf.tools.wsdlto.frontend.jaxws.generators.ClientGenerator;
+import org.apache.cxf.tools.wsdlto.frontend.jaxws.processor.WSDLToJavaProcessor;
+import org.apache.velocity.util.StringUtils;
+
+import javax.xml.namespace.QName;
+import java.util.Iterator;
+import java.util.Map;
+
+public class BonitaConnectorGenerator extends ClientGenerator {
+
+    private static String TEMPLATE_BASE = "com/ricston/bos/wsdltobonitaconnector/cxf/frontend/template";
+
+    public void generate(ToolContext penv) throws ToolException {
+        this.env = penv;
+        Map<QName, JavaModel> map = CastUtils.cast((Map) penv.get(WSDLToJavaProcessor.MODEL_MAP));
+        for (JavaModel javaModel : map.values()) {
+
+            if (javaModel.getServiceClasses().size() == 0) {
+                ServiceInfo serviceInfo = (ServiceInfo) env.get(ServiceInfo.class);
+                String wsdl = serviceInfo.getDescription().getBaseURI();
+                Message msg = new Message("CAN_NOT_GEN_CLIENT", LOG, wsdl);
+                if (penv.isVerbose()) {
+                    System.out.println(msg.toString());
+                }
+                return;
+            }
+
+            Map<String, JavaInterface> interfaces = javaModel.getInterfaces();
+            Iterator it = javaModel.getServiceClasses().values().iterator();
+            while (it.hasNext()) {
+                JavaServiceClass js = (JavaServiceClass) it.next();
+                Iterator i = js.getPorts().iterator();
+                while (i.hasNext()) {
+                    JavaPort jp = (JavaPort) i.next();
+                    String interfaceName = jp.getInterfaceClass();
+                    JavaInterface intf = interfaces.get(interfaceName);
+                    if (intf == null) {
+                        interfaceName = jp.getPortType();
+                        intf = interfaces.get(interfaceName);
+                    }
+                    Iterator<JavaMethod> methodsIterator = intf.getMethods().iterator();
+                    while (methodsIterator.hasNext()) {
+                        JavaMethod method = methodsIterator.next();
+                        String clientClassName = NameUtil.mangleNameToClassName(jp.getPortName()) + StringUtils.capitalizeFirstLetter(method.getName()) + "WS";
+
+                        clientClassName = mapClassName(intf.getPackageName(), clientClassName, penv);
+                        clearAttributes();
+                        setAttributes("clientClassName", clientClassName);
+                        setAttributes("method", method);
+                        setAttributes("intf", intf);
+                        setAttributes("service", js);
+                        setAttributes("port", jp);
+                        setAttributes("stringUtils", new StringUtils());
+
+                        setCommonAttributes();
+
+                        doWrite(TEMPLATE_BASE + "/bonita-client.vm", parseOutputName(intf.getPackageName(), clientClassName + "Connector"));
+                        doWrite(TEMPLATE_BASE + "/properties.vm", parseOutputName(intf.getPackageName(), clientClassName + "Connector", ".properties"));
+                        doWrite(TEMPLATE_BASE + "/xml.vm", parseOutputName(intf.getPackageName(), clientClassName + "Connector", ".xml"));
+                    }
+                }
+            }
+        }
+    }
+
+    private String mapClassName(String packageName, String name, ToolContext context) {
+        ClassCollector collector = context.get(ClassCollector.class);
+        int count = 0;
+        String checkName = name;
+        while (collector.containClientClass(packageName, checkName)) {
+            checkName = name + (++count);
+        }
+        collector.addClientClassName(packageName, checkName,
+                packageName + "." + checkName);
+        return checkName;
+    }
+}
